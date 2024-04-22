aeeb3984782ebabc38edb449f1dc61999d2da445
pamini <puyagit@gmail.com>
2024-04-22 13:34:26 +0330

    [*retrain] useLayoutEffect instead of useEffect + fetching data

--- src/components/TripList.js
+++ src/components/TripList.js
@@ -1,9 +1,23 @@
-import React from 'react'
-
+import React, { useLayoutEffect, useState } from 'react'
+import './TripList.css'
 export default function TripList() {
+       const [trips , setTrip] = useState([])
+       useLayoutEffect(()=> {
+              fetch('http://localhost:3000/trips').
+              then(response => response.json()).
+              then(trips => setTrip(trips))
+       })
   return (
     <div>
-      
+      <ul className='trip-list'>
+       <h2>Trip List</h2>
+       {trips.map(trip => (
+              <li key={trip.id}>
+                     <h3>{trip.title}</h3>
+                     <p>{trip.price}</p>
+                     </li>
+       ))}
+      </ul>
     </div>
   )
 }
