9a9c761671fea133d234c124421e44ffefc89124
pamini <puyagit@gmail.com>
2024-04-22 11:53:09 +0330

    Revert "reprogramming done."
    This reverts commit d33d5e6bae76e346fa70c746378849f536fc9862.

--- src/components/TripList.js
+++ src/components/TripList.js
@@ -1,33 +1,9 @@
-import React, { useState } from 'react'
-import { useFetch } from '../hooks/useFetch'
-import './TripList.css'
-
+import React from 'react'
 
 export default function TripList() {
-       const [url, setUrl] = useState('http://localhost:3000/trips')
-       const { data: trips, isPending, error } = useFetch(url, { type: 'GET' })
-       return (
-              <div className='trip-list'>
-
-       <div className='filters'>
-              <button onClick={() => setUrl('http://localhost:3000/trips?loc=usa')}>USA</button>
-              <button onClick={() => setUrl('http://localhost:3000/trips?loc=spain')}>Spain</button>
-              <button onClick={() => setUrl('http://localhost:3000/trips')}>All</button>
-              <button onClick={() => setUrl('http://localhost:3000/trips****ERROR')}>check Error</button>
-       </div>
-       <ul >
-              {isPending && <p>is loading...</p>}
-              {error && <h4 className='error'>{error}</h4>}
-              <h2>trip List</h2>
-              {!error && trips && trips.map(trip =>(
-                     <li key={trip.id}>
-                            <h3>{trip.title}</h3>
-                            <p>{trip.price}</p>
-                     </li>
-              ))}
-       </ul>
-
+  return (
+    <div>
       
     </div>
-       )
+  )
 }
